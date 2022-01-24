ee242f5773e38dfe14aa2087045ebfa3c6a31fff
Preetam Maske <preetamamaske1@gmail.com>
2022-01-24 18:08:25 +0530

    updated log changed

--- infoUpload.py
+++ infoUpload.py
@@ -6,9 +6,12 @@ import os
 def get_database():
     from pymongo import MongoClient
     import pymongo
-    CONNECTION_STRING = "mongodb+srv://admin-preetam:pam%21%40%23QW@cluster0.bq71x.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+
+    CONNECTION_STRING = "mongodb://preetam3620:preetam3620@cluster0-shard-00-00.bq71x.mongodb.net:27017,cluster0-shard-00-01.bq71x.mongodb.net:27017,cluster0-shard-00-02.bq71x.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+
     from pymongo import MongoClient
     client = MongoClient(CONNECTION_STRING)
+
     return client.IoTDB
 
 
@@ -17,22 +20,40 @@ def get_report(file):
     cycle = []
     status = True
     csvFile = csv.reader(file)
+    wft = 0     # water filling time
+    wht = 0     # water heating time
 
     for row in csvFile:
-        if row[0] == 'Status' and row[1] == 'Fail':
-            status = False
-        if (row[0].find('END') == -1): # if 'END' is not present
-            if row[0][0] == '#':
-                pass
+        if (row[0].find('WaterFillingTime') != -1):
+            wft = row[1]
+
+        elif (row[0].find('WaterHeatingTime') != -1):
+            wht = row[1]
+
+        elif (row[0].find('E') == -1): # if 'END' is not present
+            if (row[0].find('Criteria') != -1):
+                modifiedRow = [row[0], row[1] + row[2]]
+                cycle.append(modifiedRow)
             else:
                 cycle.append(row)
+
         else:
+            status = statusCheck(cycle)
+            # print(cycle, "\n")
             cycletoMap = {item[0]: item[1] for item in cycle}
             cycles.append(cycletoMap)
             cycle = []
 
-    return cycles, status
+    return cycles, status, wft, wht
 
+def statusCheck(cycle):
+    for item in cycle:
+        if(item[1] == 'Rinse'):
+            pass
+    status = 'OK'
+    statusRow = ['Status', status]
+    cycle.append(statusRow)
+    return True
 
 def uploadReport(filePath):
     currentDateTime = str(datetime.datetime.now())
@@ -46,15 +67,17 @@ def uploadReport(filePath):
     head, tail = os.path.split(filePath)
     productNumber = tail.split('.')[0]
 
-    # parse the file and get the data of cycles and status of the report
+    # parse the file and get the data of cycles
     file = open(filePath)
-    cycles, status = get_report(file)
+    cycles, status, wft, wht = get_report(file)
 
     # schema of the report 
     report = {
         'cycles': cycles,
-        'productNumber': productNumber,
         'status': status,
+        'productNumber': productNumber,
+        'wft': wft,
+        'wht': wht,
         'date': currentDate,
         'time': currentTime,
     }
--- input.py
+++ input.py
@@ -1,3 +1,3 @@
 import infoUpload
 
-infoUpload.uploadReport(filePath='./product1.csv')
+infoUpload.uploadReport(filePath='./product4.csv')
